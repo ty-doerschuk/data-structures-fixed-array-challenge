8abc5f98c6398db993c6b13a66e53a81347be5bd
Ty Doerschuk <tyler.doerschuk@gmail.com>
2016-07-29 17:06:57 -0400

    add solution

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,5 +1,29 @@
-class FixedArray
-  p 'foo'
+# FixedArray#new(size): Instantiate a new FixedArray with space for size elements.
+# FixedArray#get(index): Get a value from the array at the specified index. Throw an OutOFBoundsException if the user tries to get a value at an index outside the bounds of the fixed array.
+# FixedArray#set(index, element): Set a value in the array at a specific index and return the element. Throw an OutOFBoundsException if the user tries to set a value at an index outside the bounds of the fixed array.
 
-  p 'bar'
+class FixedArray
+
+  attr_accessor :arr
+
+  def initialize(size)
+    @arr = Array.new(size)
+  end
+
+  def set(index, element)
+    if arr.length <= index
+      raise IndexError, 'Index is not valid'
+    else
+      arr[index] = element
+    end
+  end
+
+  def get(index)
+    if arr.length <= index
+      raise IndexError, 'Index is not valid'
+    else
+      arr[index]
+    end
+  end
+
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,20 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+
+  let (:fixedarray) { FixedArray.new(5)}
+
+  it 'has an initial size of 5 elements' do
+    expect(fixedarray.arr.length).to eq 5
+  end
+
+  it '#set a value in the array at a specific index' do
+    fixedarray.set(2, 'three')
+    expect(fixedarray.arr).to eq([nil, nil, 'three', nil, nil])
+  end
+
+  it '#get a value from the array at a specific index' do
+    fixedarray.set(2, 'three')
+    expect(fixedarray.get(2)).to eq 'three'
+  end
 end
